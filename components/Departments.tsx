5fc252278aca20e239fcfeb27d0befdfcd0f7cde
nikhilganireddy <nikhilganireddy@yahoo.com>
2023-12-17 01:13:37 +0530

    first commit 4

--- components/Departments.tsx
+++ components/Departments.tsx
@@ -15,31 +15,31 @@ const Departments = () => {
         <div className={`grid grid-cols-5 gap-6 w-full`}>
             <div className={`rounded-2xl overflow-hidden relative`}>
                 <Image src={cse} alt={"cse"} width={200} height={200}
-                       className={`object-cover object-center w-full h-auto`}/>
+                       className={`object-fill object-center w-full h-auto`}/>
                 <span
                     className={`absolute bottom-0 z-20 w-full bg-white/20 backdrop-blur-md text-center py-2 text-white font-bold text-lg`}>CSE</span>
             </div>
             <div className={`rounded-2xl overflow-hidden relative`}>
                 <Image src={mining} alt={"cse"} width={200} height={200}
-                       className={`object-cover object-center w-full h-auto`}/>
+                       className={`object-fill object-center w-full h-auto`}/>
                 <span
                     className={`absolute bottom-0 z-20 w-full bg-white/20 backdrop-blur-md text-center py-2 text-white font-bold text-lg`}>MINING</span>
             </div>
             <div className={`rounded-2xl overflow-hidden relative`}>
                 <Image src={eee} alt={"cse"} width={200} height={200}
-                       className={`object-cover object-center w-full h-auto`}/>
+                       className={`object-fill object-center w-full h-auto`}/>
                 <span
                     className={`absolute bottom-0 z-20 w-full bg-white/20 backdrop-blur-md text-center py-2 text-white font-bold text-lg`}>EEE</span>
             </div>
             <div className={`rounded-2xl overflow-hidden relative`}>
                 <Image src={ece} alt={"cse"} width={200} height={200}
-                       className={`object-cover object-center w-full h-auto`}/>
+                       className={` object-fill object-center w-full h-auto`}/>
                 <span
                     className={`absolute bottom-0 z-20 w-full bg-white/20 backdrop-blur-md text-center py-2 text-white font-bold text-lg`}>ECE</span>
             </div>
             <div className={`rounded-2xl overflow-hidden relative`}>
                 <Image src={it} alt={"cse"} width={200} height={200}
-                       className={`object-cover object-center w-full h-auto`}/>
+                       className={`object-fill object-center w-full h-auto`}/>
                 <span
                     className={`absolute bottom-0 z-20 w-full bg-white/20 backdrop-blur-md text-center py-2 text-white font-bold text-lg`}>IT</span>
             </div>
